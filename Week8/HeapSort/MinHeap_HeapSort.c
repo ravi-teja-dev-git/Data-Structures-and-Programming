5393fdcb28939dfbe62aff81bc1627a86dedf929
RavitejaGundimeda <30974023+RavitejaGundimeda@users.noreply.github.com>
2022-01-11 19:05:21 +0530

    Add files via upload

---
+++ Week8/HeapSort/MinHeap_HeapSort.c
@@ -0,0 +1,81 @@
+// Heap Sort in C using Min Heap
+  
+  #include<stdio.h>
+  #include<stdlib.h>
+  
+  
+  // Swaps the elements
+  void swapElements(int *a, int *b) 
+  {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+  
+  void heapify(int arr[], int n, int i) 
+  {
+    // Find largest among root, left child and right child
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] < arr[smallest])//if left child is smaller than root
+      smallest = left;
+  
+    if (right < n && arr[right] < arr[smallest])//if right child is smaller than smallest
+      smallest = right;
+  
+    // if root is not smallest,swap with smallest and continue heapify
+    if (smallest != i) {
+      swapElements(&arr[i], &arr[smallest]);
+      heapify(arr, n, smallest);
+    }
+  }
+  
+  
+  //Heap sort
+  void heapSort(int arr[], int n) 
+  {
+  // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)//starts from non leaf node
+      heapify(arr, n, i);
+  
+    // Swap the last element and do heapify
+    for (int i = n - 1; i >= 0; i--)
+        {
+          swapElements(&arr[0], &arr[i]);
+      
+          // Heapify root element to get highest element at root again
+          heapify(arr, i, 0);
+         }   
+    
+    }
+  
+  
+  
+  
+  int main() 
+  {
+      
+    int n;
+      
+    printf("\nEnter number of elements");
+    scanf("%d",&n);
+    int arr[n];
+    
+    printf("\nEnter elements into the array ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("\n%d",&arr[i]);
+    }
+    
+    printf("\nImplemented sort using Min Heapify");
+    heapSort(arr, n);
+  
+    printf("\nArray after sorting is \n");
+    for(int i=n-1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+  }
---
+++ Week8/Quadratic_Hash/Quadratic_Hash.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<limits.h>
+#define N 20
+
+int h_table[N] = {0};
+
+//Display the hash table
+void display()
+{
+   for(int i =0; i<N; i++)
+   {
+       printf("\n At index %d key present is %d ",i,h_table[i]);
+   }
+}
+
+
+//Inserting into the hash table.
+void insert()
+{
+
+ int key,pos,i,flag=0,hkey,h1key;
+ printf("\n Enter a value to insert into hash table \n");
+ scanf("%d",&key);
+ hkey = key%N;
+ for(i=0;i<N;i++)
+    {
+
+         pos=(hkey + i* i )%N; //Hash formula for quadratic hashing
+    
+         if(h_table[pos] == 0 || h_table[pos] == INT_MAX)//If the pos is empty, we insert
+         {
+            h_table[pos]=key;
+             break;
+         }
+
+    }
+
+    if(i == N)
+     printf("\nInsertion not possible \n");
+}
+
+
+//Deleting the key in hash table
+void delete()
+{
+    int value,flag = 0;
+    printf("\nEnter key to delete : ");
+    scanf("%d",&value);
+    
+    
+    int hkey = value % N,pos;
+   // printf("Key value is %d",hkey);
+    if(h_table[hkey] == value)
+        h_table[hkey] = INT_MAX;
+        
+    //search in other positions
+    else if(h_table[hkey]!=value)
+    {
+        for(int i=0; i<N; i++)
+        {
+           pos = (hkey + i*i) % N;
+           if(h_table[pos]==value)
+           {
+            h_table[pos] = INT_MAX;
+            flag = 1;
+            
+            break;
+            }
+         }
+    
+    }
+    
+    if(flag == 0){
+        printf("%d not present in the hash table\n",value);
+    }
+     
+}
+
+
+void search()
+{
+
+ int key, pos,i,hkey;
+ printf("\nEnter element to search \n");
+ scanf("%d",&key);
+ hkey = key%N;
+ for(i=0;i<N; i++)
+ {
+    pos = (hkey+i*i)%N;
+    if(h_table[pos]==key)
+    {
+      printf("\n Key is found at position %d",pos);
+      break;
+    }
+  }
+  if(i == N)
+    printf("\n Key is not found ");
+}
+
+
+
+void loadFactor()
+{
+   float ele_count = 0, lf;
+   int i=0;
+   while(i<N)
+   {
+     if(h_table[i]!= 0 && h_table[i]!= INT_MAX)
+     ele_count+=1;
+     i++;
+   }
+   lf = ele_count / N;
+   printf("\n Load Factor is %f",lf);
+}
+
+
+
+int main()
+{
+    int choice,i;
+    while(1)
+    {
+        printf("\nEnter your choice \n1. Insert\n2. Delete \n3. Search \n4. Display \n5. Load Factor \n6. Exit \n ");
+        scanf("%d",&choice);
+        if(choice == 1)
+        {
+           insert();
+           
+        }
+        else if(choice == 2)
+        {
+            delete();
+            
+        }
+        else if(choice == 3)
+        {
+            search();
+            
+        }
+        else if(choice == 4)
+        {
+             display();
+             
+        }
+        else if(choice == 5)
+        {
+             loadFactor();
+             
+        }
+        else if(choice == 6)
+        {
+            exit(1);
+            
+        }
+        
+    }
+}
+
